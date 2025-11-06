d3bfe5434c8f72b1c2c6569c822289299231c536
Supakit Jaratworapat <supakit.jaratworapat@gmail.com>
2025-11-06 11:14:17 +0700

    Add Car class with methods for setting brand and speed, and display functionality in CreateYourOwnClass.java

--- CreateYourOwnClass/CreateYourOwnClass.java
+++ CreateYourOwnClass/CreateYourOwnClass.java
@@ -1,5 +1,38 @@
 package CreateYourOwnClass;
 
 public class CreateYourOwnClass {
-    
+    public static void main(String[] args) {
+        class Car {
+            private String brand;
+            private double speed;
+
+            public void setSpeed(double speed) {
+                if (speed >= 50) {
+                    this.speed = speed;
+                } else {
+                    System.out.println("A standard car speed must be greater than 50 km/h");
+                }
+            }
+
+            public void setBrand(String brand) {
+                this.brand = brand;
+            }
+
+            public String getBrand() {
+                return brand;
+            }
+
+            public double getSpeed() {
+                return speed;
+            }
+
+            void displayInfo() {
+                System.out.println("Car brand: " + getBrand() + ", Speed: " + getSpeed() + " km/h");
+            }
+        }
+        Car Car1 = new Car();
+        Car1.setBrand("Toyota");
+        Car1.setSpeed(50);
+        Car1.displayInfo();
+    }
 }
